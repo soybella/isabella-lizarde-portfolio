5d23234fff9524910e497c1f91e8d27bdc2d0a96
Isabella <isabellaj.lizarde@gmail.com>
2023-08-03 03:03:02 -0700

    Updated about me section, added temporary photo

--- src/Main.js
+++ src/Main.js
@@ -2,15 +2,16 @@ import React from "react";
 import Skills from "./Skills.js";
 import Projects from "./Projects.js";
 import Contact from "./Contact.js";
+import profilePhoto from "./images/temp-profile-photo.jpeg";
 
 export default function Main() {
   return (
     <div className="Main">
-      <div class="container pt-5">
+      <div class="container about-content pt-5">
         <div class="row">
-          <div class="col-sm-6 about-content">
+          <div class="col-sm">
             <h2 class="mb-3">Hi I am Isabella Lizarde</h2>
-            <h4 class="mb-3">Front-end developer, based in California</h4>
+            <h4 class="mb-3">Front-end developer</h4>
             <p>
               Far far away, behind the word mountains, far from the countries
               Vokalia and Consonantia, there live the blind texts. Separated
@@ -25,7 +26,7 @@ export default function Main() {
           </div>
           <div class="col-md-6 d-md-block">
             <img
-              src="/images/profile-image.jpg"
+              src={profilePhoto}
               class="img-fluid portfolio-profile-image"
               alt="Isabella's Portfolio Profile Image"
             />
