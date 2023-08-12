f6c0fa47c571e725e06b8ea70ef755b0daaf5538
Isabella <isabellaj.lizarde@gmail.com>
2023-08-11 22:33:30 -0700

    Updated skills section layout

--- src/Main.js
+++ src/Main.js
@@ -13,27 +13,30 @@ export default function Main() {
             <div className="about-header">
               <h2 className="mb-4">About Me</h2>
               <h5 className="mb-3">Hello World!</h5>
+            </div>
+            <div className="about-bio">
               <p>
                 My name is Isabella Lizarde, and I'm a Front-end Web Developer
                 based in California 🇺🇸 <br />
               </p>
-            </div>
-            <div className="about-bio">
               <p>
                 I've always had a passion for creating beautiful works of art
                 and bringing things to life visually. As a former art major, I
                 believe that Front-end development is a perfect combination of
                 art and logic, and I truly enjoy creating engaging, user
-                friendly websites. <br /> When I'm not coding, I like to spend
-                my free time with my dogs, listening to music, or catching up on
-                the most recent video game I've been playing!
+                friendly websites. <br />
+              </p>
+              <p>
+                When I'm not coding, I like to spend my free time with my dogs,
+                grabbing a coffee, or catching up on the most recent video game
+                I've been playing.
               </p>
             </div>
           </div>
           <div className="col-lg-6 d-none d-lg-block">
             <img
               src={profilePhoto}
-              className="img-fluid portfolio-profile-image"
+              className="img-fluid rounded portfolio-profile-image"
               alt="Isabella Lizarde"
             />
           </div>
--- src/Skills.js
+++ src/Skills.js
@@ -1,5 +1,75 @@
 import React from "react";
+import vscodeIcon from "./images/vscode-icon.png";
+import responsiveIcon from "./images/responsive-icon.jpeg";
 
 export default function Skills() {
-  return <div className="Skills"></div>;
+  return (
+    <div className="Skills">
+      <div className="pt-3 skills-wrapper">
+        {/* <div className="skills-container justify-content-center"> */}
+        <h2 className="text-center">Skills</h2>
+        <div className="skills-container justify-content-center">
+          <div className="col-md-4 text-center">
+            <div className="skills-icon">
+              <i className="fa-brands fa-html5"></i>
+              <h4>HTML5</h4>
+            </div>
+            <div className="skills-icon">
+              <i className="fa-brands fa-css3-alt"></i>
+              <h4>CSS</h4>
+            </div>
+            <div className="skills-icon">
+              <i className="fa-brands fa-square-js"></i>
+              <h4>JavaScript</h4>
+            </div>
+            <div className="skills-icon">
+              <i className="fa-solid fa-gear"></i>
+              <h4>API</h4>
+            </div>
+          </div>
+          <div className="col-md-4 text-center">
+            <div className="skills-icon">
+              <i className="fa-brands fa-react"></i>
+              <h4>React</h4>
+            </div>
+            <div className="skills-icon">
+              <i className="fa-brands fa-github"></i>
+              <h4>GitHub</h4>
+            </div>
+            <div className="skills-icon">
+              <div className="img-icon vscode-icon">
+                <img src={vscodeIcon} alt="vscode-icon" />
+              </div>
+              <h4>VSCode</h4>
+            </div>
+            <div className="skills-icon">
+              <i className="fa-solid fa-terminal"></i>
+              <h4>Terminal</h4>
+            </div>
+          </div>
+
+          <div className="col-md-4 text-center">
+            <div className="skills-icon">
+              <i className="fa-brands fa-bootstrap"></i>
+              <h4>Bootstrap</h4>
+            </div>
+            <div className="skills-icon">
+              <div className="img-icon responsive-icon">
+                <img src={responsiveIcon} alt="responsive-icon" />
+              </div>
+              <h4>Responsive Design</h4>
+            </div>
+            <div className="skills-icon">
+              <i className="fa-brands fa-npm"></i>
+              <h4>NPM</h4>
+            </div>
+            <div className="skills-icon">
+              <i className="fa-solid fa-magnifying-glass"></i>
+              <h4>SEO</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
