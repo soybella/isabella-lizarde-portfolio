0526fc5f9ed171bead99e2413ab0f4b5cf128f59
Isabella <isabellaj.lizarde@gmail.com>
2023-08-19 01:51:41 -0700

    Updated projects section and added a new portfolio project

--- src/Projects.js
+++ src/Projects.js
@@ -3,6 +3,7 @@ import dictionaryProject from "./images/dictionary-project.png";
 import weatherProject from "./images/weather-project.png";
 import travelSiteProject from "./images/responsive-travel-site.png";
 import responsiveIcon from "./images/responsive-icon.jpeg";
+import worldClockProject from "./images/world-clock.png";
 
 export default function Projects() {
   return (
@@ -18,7 +19,7 @@ export default function Projects() {
               alt="#"
             />
           </div>
-          <div className="overlay d-sm-block d-lg-none project-description">
+          <div className="responsive-project-description d-sm-block d-lg-none project-description">
             <h3 className="mt-3">Dictionary App</h3>
             <p className="p-2 m-3 text-center">
               A simple React dictionary app with a search engine that uses API
@@ -197,6 +198,56 @@ export default function Projects() {
               alt="#"
             />
           </div>
+          <div className="responsive-project-description d-sm-block d-lg-none project-description">
+            <h3 className="mt-3">Weather App</h3>
+            <p className="p-2 m-3 text-center">
+              A React weather app with a search engine that displays live
+              weather for the current day, as well as a five day forecast using
+              API integration.
+            </p>
+            <div className="project-skills-icons pt-2">
+              <ul className="d-flex p-0 justify-content-center">
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-html5"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-css3-alt"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-square-js"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-solid fa-gear"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-bootstrap"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-react"></i>
+                </li>
+              </ul>
+              <ul className="d-flex justify-content-evenly project-button pt-3">
+                <li className="project-button">
+                  <a
+                    href="mailto:isabellaj.lizarde@gmail.com"
+                    className="button-link work-inquiry-link"
+                    title="Work Inquiry"
+                  >
+                    View Live
+                  </a>
+                </li>
+                <li className="project-button">
+                  <a
+                    href="mailto:isabellaj.lizarde@gmail.com"
+                    className="button-link work-inquiry-link"
+                    title="Work Inquiry"
+                  >
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -210,6 +261,56 @@ export default function Projects() {
               alt="#"
             />
           </div>
+          <div className="responsive-project-description d-sm-block d-lg-none project-description">
+            <h3 className="mt-3">Travel Site</h3>
+            <p className="p-2 m-3 text-center">
+              A React weather app with a search engine that displays live
+              weather for the current day, as well as a five day forecast using
+              API integration.
+            </p>
+            <div className="project-skills-icons pt-2">
+              <ul className="d-flex p-0 justify-content-center">
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-html5"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-css3-alt"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-square-js"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-solid fa-gear"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-bootstrap"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-react"></i>
+                </li>
+              </ul>
+              <ul className="d-flex justify-content-evenly project-button pt-3">
+                <li className="project-button">
+                  <a
+                    href="mailto:isabellaj.lizarde@gmail.com"
+                    className="button-link work-inquiry-link"
+                    title="Work Inquiry"
+                  >
+                    View Live
+                  </a>
+                </li>
+                <li className="project-button">
+                  <a
+                    href="mailto:isabellaj.lizarde@gmail.com"
+                    className="button-link work-inquiry-link"
+                    title="Work Inquiry"
+                  >
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
           <div className="col-lg-6 d-none d-lg-block project-description">
             <h3 className="mb-3">Travel Site</h3>
             <p className="p-2 m-0">
@@ -218,7 +319,7 @@ export default function Projects() {
               responsive design.
             </p>
             <div className="project-skills-icons pt-2">
-              <ul className="d-flex p-0 justify-content-start">
+              <ul className="d-flex p-0 justify-content-center">
                 <p className="p-2">Technologies used: </p>
                 <li className="skills-icon p-2">
                   <i className="fa-brands fa-html5"></i>
@@ -236,6 +337,102 @@ export default function Projects() {
                 </li>
               </ul>
               <ul className="d-flex justify-content-evenly project-button">
+                <li className="project-button">
+                  <a
+                    href="mailto:isabellaj.lizarde@gmail.com"
+                    className="button-link work-inquiry-link"
+                    title="Work Inquiry"
+                  >
+                    View Live
+                  </a>
+                </li>
+                <li className="project-button">
+                  <a
+                    href="mailto:isabellaj.lizarde@gmail.com"
+                    className="button-link work-inquiry-link"
+                    title="Work Inquiry"
+                  >
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* project 4 */}
+      <div className="container projects-container px-3">
+        <div className="row">
+          <div className="col-lg-6 d-none d-lg-block project-description">
+            <h3 className="mb-3">World Clock</h3>
+            <p className="p-2 m-0">
+              A simple React dictionary app with a search engine that uses API
+              integration to look up the definition of a word along with a
+              gallery of relative photos.
+            </p>
+            <div className="project-skills-icons pt-2">
+              <ul className="d-flex p-0 justify-content-center">
+                <p className="p-2">Technologies used: </p>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-html5"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-css3-alt"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-square-js"></i>
+                </li>
+              </ul>
+              <ul className="d-flex justify-content-evenly project-button">
+                <li className="project-button">
+                  <a
+                    href="mailto:isabellaj.lizarde@gmail.com"
+                    className="button-link work-inquiry-link"
+                    title="Work Inquiry"
+                  >
+                    View Live
+                  </a>
+                </li>
+                <li className="project-button">
+                  <a
+                    href="mailto:isabellaj.lizarde@gmail.com"
+                    className="button-link work-inquiry-link"
+                    title="Work Inquiry"
+                  >
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-6 portfolio-project-image d-md-block">
+            <img
+              src={worldClockProject}
+              className="img-fluid portfolio-project-image border-0"
+              alt="#"
+            />
+          </div>
+          <div className="responsive-project-description d-sm-block d-lg-none project-description">
+            <h3 className="mt-3">World Clock</h3>
+            <p className="p-2 m-3 text-center">
+              A React weather app with a search engine that displays live
+              weather for the current day, as well as a five day forecast using
+              API integration.
+            </p>
+            <div className="project-skills-icons pt-2">
+              <ul className="d-flex p-0 justify-content-center">
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-html5"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-css3-alt"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-square-js"></i>
+                </li>
+              </ul>
+              <ul className="d-flex justify-content-evenly project-button pt-3">
                 <li className="project-button">
                   <a
                     href="mailto:isabellaj.lizarde@gmail.com"
