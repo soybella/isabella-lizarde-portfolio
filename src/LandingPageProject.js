57c2cf0b117bc62941731d30c92394446ff2db3f
Isabella <isabellaj.lizarde@gmail.com>
2024-04-23 02:07:14 -0700

    Created landing page project component

---
+++ src/LandingPageProject.js
@@ -0,0 +1,119 @@
+import React from "react";
+import landingPageProject from "./images/landing-page-project.png";
+import "./Projects.css";
+
+export default function LandingPageProject() {
+  return (
+    <article className="LandingPageProject">
+      <section className="container projects-container">
+        <div className="row">
+          <article className="col-lg-6 d-none d-lg-block project-description">
+            <h3 className="p-2 mb-3 text-end">Landing Page</h3>
+            <p className="p-2 m-0 text-end">
+              A Company landing page made with vanilla HTML and CSS only to
+              strengthen UI and responsive skills. Challenge is from{" "}
+              <a
+                href="https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db"
+                className="challenge-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Frontend Mentor
+              </a>
+              .
+            </p>
+            <div className="project-skills-icons pt-4">
+              <ul className="d-flex p-0 justify-content-end">
+                <p className="p-2">Technologies: </p>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-html5"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-css3-alt"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i class="fa-solid fa-mobile-screen-button"></i>
+                </li>
+              </ul>
+              <ul className="d-flex justify-content-end project-button">
+                <li className="project-button pe-3">
+                  <a
+                    href="https://isabellalizarde-company-landing-page.netlify.app"
+                    className="button-link"
+                    title="Landing Page Website"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    View Live
+                  </a>
+                </li>
+                <li className="project-button">
+                  <a
+                    href="https://github.com/soybella/isabellalizarde-company-landing-page"
+                    className="button-link"
+                    title="Landing Page Project Github Repository"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <div className="col-md-6 portfolio-project-image d-md-block">
+            <img
+              src={landingPageProject}
+              className="img-fluid portfolio-project-image border-0"
+              alt="#"
+            />
+          </div>
+          <article className="responsive-project-description d-sm-block d-lg-none project-description pt-3">
+            <h3 className="mt-3">Landing Page</h3>
+            <p className="p-2 m-3 text-center">
+              A web app that uses advanced elements of JavaScript to create
+              display a clock with times from different places around the world.
+            </p>
+            <div className="project-skills-icons pt-2">
+              <ul className="d-flex p-0 justify-content-center">
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-html5"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-css3-alt"></i>
+                </li>
+                <li className="skills-icon p-2">
+                  <i className="fa-brands fa-square-js"></i>
+                </li>
+              </ul>
+              <ul className="d-flex justify-content-center responsive-project-button project-button pt-3">
+                <li className="project-button">
+                  <a
+                    href="https://isabellalizarde-company-landing-page.netlify.app"
+                    className="button-link "
+                    title="Landing Page Website"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    View Live
+                  </a>
+                </li>
+                <li className="project-button">
+                  <a
+                    href="https://github.com/soybella/isabellalizarde-company-landing-page"
+                    className="button-link"
+                    title="Landing Page Github Repository"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+        </div>
+      </section>
+    </article>
+  );
+}
